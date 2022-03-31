c24303c6773ca712355669b3bbb6da48b909f70e
Amber Ellett <80650755+naitheas@users.noreply.github.com>
2022-03-31 14:31:17 -0600

    Add files via upload

---
+++ 39.8ColorBoxMaker/todo_list_app/src/Components/Todo.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import EditTodoForm from './EditTodoForm';
+
+const Todo = ({ name, remove, editTodo, idx, id, handleCheck, isComplete }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+  const classes = isComplete ? 'Todo-complete' : 'Todo-incomplete';
+  return (
+    <div className="Todo">
+      {!isEdit && (
+        <div>
+          <span className={classes}>{name}</span>
+
+          <button className="Todo-btn" onClick={toggleEdit}>
+            Edit
+          </button>
+          <button className="Todo-btn" onClick={handleCheck}>
+            Mark as Completed
+          </button>
+          <button className="Todo-btn" onClick={remove}>
+            Remove
+          </button>
+        </div>
+      )}
+      {isEdit && (
+        <EditTodoForm
+          toggleEdit={toggleEdit}
+          editTodo={editTodo}
+          todoName={name}
+          id={id}
+          idx={idx}
+          isComplete={isComplete}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Todo;
