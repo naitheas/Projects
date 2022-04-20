d00146c718c3858bbc9f7137170b6a549afcaca0
Amber Ellett <80650755+naitheas@users.noreply.github.com>
2022-04-20 13:17:08 -0600

    Add files via upload

---
+++ 43 React-Jobly/react-jobly/jobly-frontend/build/precache-manifest.165861c940032dbc53c4c4929a1e877b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da89e945acb74b28e1d2cfa5eb535210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05ab8a892bcb262127f6",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "a6f0b3e53debce5063bf",
+    "url": "/static/css/main.f40adb53.chunk.css"
+  },
+  {
+    "revision": "05ab8a892bcb262127f6",
+    "url": "/static/js/2.e3738b6a.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/static/js/2.e3738b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f0b3e53debce5063bf",
+    "url": "/static/js/main.e4459f9b.chunk.js"
+  },
+  {
+    "revision": "96d14116823ca0bb5594",
+    "url": "/static/js/runtime-main.159b7ccd.js"
+  }
+]);
